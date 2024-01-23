e52a02957d4b03a1a13c7db797d53a58830fefe3
jacktrusler <jtrus93@gmail.com>
2024-01-23 12:12:49 -0500

    adding node apr to dashboard

--- dashboards/nodeApr.ts
+++ dashboards/nodeApr.ts
@@ -1,17 +1,52 @@
 import type { Metrics, ReturnedMetric } from "../lib/types.ts";
-import { MINIPOOL_STATUS_MAP } from "../lib/utils/utils.js";
-import { BigNumber, utils } from "https://esm.sh/ethers@5.7.2?dts";
 
 const minNodeApr: Metrics = {
   type: "rest",
   metric: {
+    method: "GET",
     source: "gogo-api",
     name: "minNodeApr",
     title: "Minimum Node Apr Value",
     desc: "The minimum APR you will get as a node operator at 10% collateralization",
-    path: "/calculator"
-    body: {
-      ggp
+    path: "/nodeOpSpread",
+    formatter: async (
+      m: Metrics,
+      value: Response,
+    ): Promise<ReturnedMetric> => {
+      // get the raw body of the response
+      const body = await value.json();
+      return {
+        name: m.metric.name,
+        title: m.metric.title,
+        desc: m.metric.desc,
+        value: body.min.apy,
+      };
     }
   }
+}
 
+const maxNodeApr: Metrics = {
+  type: "rest",
+  metric: {
+    method: "GET",
+    source: "gogo-api",
+    name: "maxNodeApr",
+    title: "Maximum Node Apr Value",
+    desc: "The maximum APR you will get as a node operator at 150% collateralization",
+    path: "/nodeOpSpread",
+    formatter: async (
+      m: Metrics,
+      value: Response,
+    ): Promise<ReturnedMetric> => {
+      const body = await value.json();
+      return {
+        name: m.metric.name,
+        title: m.metric.title,
+        desc: m.metric.desc,
+        value: body.max.apy,
+      };
+    },
+  }
+}
+
+export default [minNodeApr, maxNodeApr]
